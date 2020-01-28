19aef7691c32ec613c81ff10a90c000011614393
Jacky Lin <jacky.jia.lin@gmail.com>
2020-01-28 18:21:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-  Enumerator.new do |y|
-    array.each { |e| y << e ** 2 }    
-  end
-  .take(array.length)
+  counter = 0 
+while array[counter] do 
+  puts array[counter]
+  counter += 1 
 end
+end 
