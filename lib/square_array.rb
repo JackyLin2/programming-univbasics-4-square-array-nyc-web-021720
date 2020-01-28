c2bcb85ce15d9e52a2ce8228b196b84216522b04
Jacky Lin <jacky.jia.lin@gmail.com>
2020-01-28 18:31:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,6 @@
 def square_array(array)
-
- new_array = []
-
  array.each do |integer|
-  new_array << integer ** 2
+  new_array = [integer ** 2, integer **2, integer **2]
+  return new_array
  end
- return new_array
 end
